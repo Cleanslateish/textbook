69eb0f12f248dd32ee3d91d2e498412868917b3a
Mihailo Mladenovic <mihailo.d.mladenovic@gmail.com>
2024-08-07 14:57:02 +0200

    Barely anything, couldnt focus.

--- 002/main.c
+++ 002/main.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void clear_input_buffer(){
+  int c;
+  while((c = getchar()) != '\n' && c != EOF){}
+  
+}
+
+void add_contact(){
+  clear_input_buffer();
+  char contact_info_buffer[100];
+
+  printf("$|Contact name: ");
+  if(fgets(contact_info_buffer, sizeof(contact_info_buffer), stdin)  != NULL){
+    
+    printf("You entered: %s", contact_info_buffer);
+
+  }else{
+    printf("Error reading input.\n");
+  }
+}
+
+
+
+
 
 
 void main_menu(){
@@ -21,6 +46,7 @@ void main_menu(){
 
     case 1:
       printf("$|Add Contact-x\n");
+      add_contact();
       break;
 
     case 2:
@@ -43,13 +69,6 @@ void main_menu(){
 
 }
 
-void add_contact(){
-  char contact_info_buffer[100];
-  if(fgets(contact_info_buffer, sizeof(contact_info_buffer), stdin)  != NULL){
-    
-    
-  }
-}
 
 
 
