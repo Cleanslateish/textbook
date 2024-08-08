1f9cae24edff3c22f7ca5408f9307dc8d1f8c172
Mihailo Mladenovic <mihailo.d.mladenovic@gmail.com>
2024-08-08 23:53:57 +0200

    One tinsy wincy step for man...

--- 002/main.c
+++ 002/main.c
@@ -15,6 +15,12 @@ void clear_input_buffer(){
   while((c = getchar()) != '\n' && c != EOF){} 
 }
 
+void press_any_key_to_continue(){
+  printf("$|Press any key to return to the main menu: ");
+  getchar();
+  printf("\n");
+}
+
 bool is_all_digits(const char* str) {
     while (*str) {
         if (*str == '\n') { 
@@ -53,7 +59,8 @@ bool is_valid_email(const char* email) {
 int get_user_input_contact_name(char* name){
   printf("$|Contact Name: ");
   if(fgets(name, 50, stdin)  == NULL){
-    printf("\nError reading name input\n");
+    printf("\n$|Error reading name input.\n");
+    press_any_key_to_continue();
     return 1;
   }
 
@@ -61,7 +68,8 @@ int get_user_input_contact_name(char* name){
   
   for (size_t i = 0; i < strlen(name); i++){
     if(!isalpha(name[i]) && name[i] != ' '){
-      printf("\nName must contain only letters and spaces\n");
+      printf("\n$|Name must contain only letters and spaces.\n");
+      press_any_key_to_continue(); 
       return 1;
     }
   }
@@ -75,14 +83,16 @@ int get_user_input_contact_name(char* name){
 int get_user_input_contact_phone_number(char* phoneNumber){
   printf("$|Contact Phone Number: ");
   if(fgets(phoneNumber, 50, stdin)  == NULL){
-    printf("\nError reading phone number input\n");
+    printf("\n$|Error reading phone number input.\n");
+    press_any_key_to_continue();
     return 1;
   }
 
   phoneNumber[strcspn(phoneNumber, "\n")] = '\0';
 
   if(!is_all_digits(phoneNumber)){
-    printf("\nPhone number must contain only digits\n");
+    printf("\n$|Phone number must contain only digits.\n");
+    press_any_key_to_continue();
     return 1;
   }
 
@@ -95,13 +105,15 @@ int get_user_input_contact_phone_number(char* phoneNumber){
 int get_user_input_contact_email(char* email){
   printf("$|Contact Email: ");
   if(fgets(email, 100, stdin)  == NULL){
-    printf("\nError reading email input\n");
+    printf("\n$|Error reading email input.\n");
+    press_any_key_to_continue();
   }
 
   email[strcspn(email, "\n")] = '\0';
 
   if(!is_valid_email(email)){
-    printf("\nEmail must end with @gmail.com\n");
+    printf("\n$|Email must end with @gmail.com.\n");
+    press_any_key_to_continue();
     return 1;
   }
 
@@ -132,11 +144,11 @@ int add_contact(){
   }
   
 
-  FILE *file = fopen("contacts.txt", "a");
+  FILE* file = fopen("contacts.txt", "a");
 
   if (file == NULL){
 
-    printf("Error opening file!\n");
+    printf("$|Error opening file!\n");
     return 1;
   }
 
@@ -145,14 +157,81 @@ int add_contact(){
     fprintf(file, "%s%s",   "Phone Number: " , contact.phoneNumber);
     fprintf(file, "%s%s\n", "Email: ",         contact.email);
   }else{
-    printf("One or more input fields are invalid. Contact not saved\n");
+    printf("$|One or more input fields are invalid. Contact not saved.\n");
   }
 
   fclose(file);
-    
   return 0;
 }
 
+int view_contacts(){
+  FILE* file = fopen("contacts.txt", "r");
+  char fileBuffer[256];
+  
+
+  if(file == NULL){
+    printf("$|Error opening file!\n");
+    return 1;
+  }
+
+
+  while(fgets(fileBuffer, sizeof(fileBuffer), file) != NULL){
+    printf("%s", fileBuffer);
+  }
+
+  fclose(file);
+
+  return 0;
+}
+
+int delete_contact(){
+  char delete_contact_buffer[256];
+  view_contacts();
+  printf("\n$|Please input the name of the contact you wish to delete: ");
+  
+  if(fgets(delete_contact_buffer, 256, stdin)  == NULL){
+    printf("\n$|Error reading delete contact input.\n");
+    press_any_key_to_continue();
+  }
+
+  FILE* file = fopen("contacts.txt", "r");
+  if(file == NULL){
+    printf("$|Error opening file.");
+    return 1;
+  }
+
+  FILE* tempFile = fopen("temp.txt", "w");
+  if(tempFile == NULL){
+    printf("$|Error creating temporary file.");
+    fclose(file);
+    return 1;
+  }
+
+  char line[256];
+  while(fgets(line, sizeof(line), file)){
+    if(strstr(line, delete_contact_buffer) == NULL){
+      fputs(line, tempFile);
+    }
+  }
+
+
+
+  fclose(file);
+  fclose(tempFile);
+
+  if(remove("contacts.txt") != 0){
+      printf("Error deleting the original file.");
+      return 1;
+  }
+
+  if(rename("temp.txt", "contacts.txt") != 0){
+    printf("Error renaming the temporary file.");
+    return 1;
+  }
+
+
+  return 0;
+}
 
 void main_menu(){
   int choice = 10;
@@ -161,9 +240,8 @@ void main_menu(){
     printf("2|View Contacts\n");
     printf("3|Delete Contact\n");
     printf("0|Exit\n");
-    printf("\n$|Please input a  number from the list on screen.\n");
+    printf("\n$|Please input a number from the list on screen.\n");
 
-    printf("\n");
     printf("$|Choice: ");
     scanf("%d", &choice); 
     printf("\n");
@@ -171,8 +249,7 @@ void main_menu(){
 
     switch(choice){
       case 0:
-        printf("\n");
-        printf("$|Exiting-x\n");
+        printf("\n$|Exit-x\n");
         return;
       case 1:
         printf("$|Add Contact-x\n");
@@ -181,16 +258,18 @@ void main_menu(){
 
       case 2:
         printf("$|View Contacts-x\n");
+        view_contacts();
+        press_any_key_to_continue();
         break;
 
       case 3:
         printf("$|Delete Contact-x\n");
+        delete_contact();
         break;
 
       default: 
         printf("\n");
-        printf("$| Please input a number from the shown list|\n");
-        printf("\n");
+        printf("\n$|Please input a number from the list on screen.\n");
         while((choice = getchar() != '\n' && choice != EOF)){}
         break;
             
