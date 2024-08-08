e48becda0b3802346cd3ea0b0ea5d78f023fb156
Mihailo Mladenovic <mihailo.d.mladenovic@gmail.com>
2024-08-08 09:00:47 +0200

    Finally remember structs exist...

--- 002/main.c
+++ 002/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void clear_input_buffer(){
   int c;
@@ -7,18 +8,81 @@ void clear_input_buffer(){
   
 }
 
+struct Contact{
+  char name[50];
+  char phoneNumber[50];
+  char email[100];
+};
+
+
+
 void add_contact(){
   clear_input_buffer();
   char contact_info_buffer[100];
+  struct Contact contact1;
+  
 
-  printf("$|Contact name: ");
+  printf("$|Contact Name: ");
   if(fgets(contact_info_buffer, sizeof(contact_info_buffer), stdin)  != NULL){
+    printf("\n");
     
-    printf("You entered: %s", contact_info_buffer);
-
+    //Remove the newline charcter if it exists
+    contact_info_buffer[strcspn(contact_info_buffer, "\n")] = '\0';
+    
+    //Add newline character (fgets doesn't do this for us)
+    strcat(contact_info_buffer, "\n");
   }else{
     printf("Error reading input.\n");
   }
+  strcpy(contact1.name, contact_info_buffer); 
+  
+
+
+  printf("$|Contact Phone Number: ");
+  if(fgets(contact_info_buffer, sizeof(contact_info_buffer), stdin)  != NULL){
+    printf("\n");
+    
+    //Remove the newline charcter if it exists
+    contact_info_buffer[strcspn(contact_info_buffer, "\n")] = '\0';
+    
+    //Add newline character (fgets doesn't do this for us)
+    strcat(contact_info_buffer, "\n");
+  }else{
+    printf("Error reading input.\n");
+  }
+  strcpy(contact1.phoneNumber, contact_info_buffer); 
+  
+  printf("$|Contact Email: ");
+  if(fgets(contact_info_buffer, sizeof(contact_info_buffer), stdin)  != NULL){
+    printf("\n");
+    
+    //Remove the newline charcter if it exists
+    contact_info_buffer[strcspn(contact_info_buffer, "\n")] = '\0';
+    
+    //Add newline character (fgets doesn't do this for us)
+    strcat(contact_info_buffer, "\n");
+  }else{
+    printf("Error reading input.\n");
+  }
+  strcpy(contact1.email, contact_info_buffer); 
+
+
+  FILE *file = fopen("contacts.txt", "a");
+
+  if (file == NULL){
+
+    printf("Error opening file!\n");
+    return;
+  }
+
+  fprintf(file, "%s", contact1.name);
+  fprintf(file, "%s", contact1.phoneNumber);
+  fprintf(file, "%s\n", contact1.email);
+
+  fclose(file);
+
+    
+
 }
 
 
@@ -34,9 +98,11 @@ void main_menu(){
   printf("0|Exit\n");
   printf("\n");
 
-  printf("$|Please type in a number from the list on screen.\n");
+  printf("$|Please input a  number from the list on screen.\n");
+  printf("\n");
   printf("$|Choice: ");
   scanf("%d", &choice); 
+  printf("\n");
 
   switch(choice){
     case 0:
@@ -47,6 +113,7 @@ void main_menu(){
     case 1:
       printf("$|Add Contact-x\n");
       add_contact();
+      main_menu();
       break;
 
     case 2:
